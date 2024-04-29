b2a92e0c4f5168b3dd3a95a6914427086b6fde8c
Mathieu Morissette <mmorissette2@cegepthetford.ca>
2024-04-29 09:29:04 -0400

    Ajoutez des fichiers projet.

---
+++ Podo_Affaires/Podometre.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podo_Affaires
+{
+    public class Podometre
+    {
+        private int _valeur;
+        public int Valeur { get { return _valeur; } set { _valeur = value; } }
+        public Podometre()
+        {
+            _valeur = 0;
+        }
+        public void incrementer()
+        {
+            _valeur++;
+        }
+        public void decrementer()
+        {
+            if (_valeur != 0)
+                _valeur--;
+        }
+        public void raz()
+        {
+            _valeur = 0;
+        }
+    }
+}
+            
---
+++ Podo_UI/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Podo_Affaires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Podo_UI
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Podometre _podo;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _podo = new Podometre();
+        }
+
+        private void BtnIncrementer_Click(object sender, RoutedEventArgs e)
+        {
+            _podo.incrementer();
+            tbValeur.Text = Convert.ToString(_podo.Valeur);
+        }
+
+        private void BtnDecrementer_Click(object sender, RoutedEventArgs e)
+        {
+            _podo.decrementer();
+            tbValeur.Text = Convert.ToString(_podo.Valeur);
+        }
+
+        private void BtnRaz_Click(object sender, RoutedEventArgs e)
+        {
+            _podo.raz();
+            tbValeur.Text = "0";
+        }
+    }
+}
